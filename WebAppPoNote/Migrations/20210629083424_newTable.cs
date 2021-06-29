2d565644c8c40adb13905aa95506eba6217c868a
Vera <mar0sis@gmail.com>
2021-06-29 11:52:05 +0300

    Add project files.

---
+++ WebAppPoNote/Migrations/20210629083424_newTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppPoNote.Migrations
+{
+    public partial class newTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebAppPoNote/Models/AppNote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPoNote.Models
+{
+    public class AppNote
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string ImageURL { get; set; }
+        public bool priority { get; set; }
+        public bool isActive { get; set; }
+    }
+
+}
